c23e8c5c8dbfb2cf0ff15838e19cd715fa0ca444
Bartlomiej <kulik.bartlomiej@gmail.com>
2018-04-14 22:26:15 +0200

    code format

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int K; // number of services
 int P; // priority vaule
 int L; // number of visits in services
 
-// shared memory below
+       // shared memory below
 int *inService;
 int *readyToEntry;
 int *readyToEscape;
@@ -27,8 +27,8 @@ int *freeServiceTrack;
 // end of shared memory
 // semaphores
 int semId;
-struct sembuf semOpArg = {0, 0, 0};
-enum { acces, queueEntry, queueExit};
+struct sembuf semOpArg = { 0, 0, 0 };
+enum { acces, queueEntry, queueExit };
 // key to semaphores and shared memory
 key_t key;
 
@@ -44,99 +44,99 @@ void V(int semaphore);
 // MAIN FUNCTION
 int main(int argc, char **argv)
 {
-  // set N,K,P,L
-	if(!areValid(argc,argv))
-  {
-    return 1;
-  }
+    // set N,K,P,L
+    if (!areValid(argc, argv))
+    {
+        return 1;
+    }
 
-	startWork();
-	if(runTasks())
-  {
+    startWork();
+    if (runTasks())
+    {
+        return 0;
+    }
+    endWork();
+
     return 0;
-  }
-  endWork();
-
-  return 0;
 }
 
 void startWork()
 {
-  int shmId;
-  // MEMORY
-  shmId = shmget(key, sizeof(int), 0600 | IPC_CREAT);
-  inService = (int *)shmat(shmId, NULL, 0);
-  *inService = 0;
-  shmdt(inService);
+    int shmId;
+    // MEMORY
+    shmId = shmget(key, sizeof(int), 0600 | IPC_CREAT);
+    inService = (int *)shmat(shmId, NULL, 0);
+    *inService = 0;
+    shmdt(inService);
 
-  shmId = shmget(key + 1, sizeof(int), 0600 | IPC_CREAT);
-  readyToEntry = (int *)shmat(shmId, NULL, 0);
-  *readyToEntry = 0;
-  shmdt(readyToEntry);
+    shmId = shmget(key + 1, sizeof(int), 0600 | IPC_CREAT);
+    readyToEntry = (int *)shmat(shmId, NULL, 0);
+    *readyToEntry = 0;
+    shmdt(readyToEntry);
 
-  shmId = shmget(key + 2, sizeof(int), 0600 | IPC_CREAT);
-  readyToEscape = (int *)shmat(shmId, NULL, 0);
-  *readyToEscape = 0;
-  shmdt(readyToEscape);
+    shmId = shmget(key + 2, sizeof(int), 0600 | IPC_CREAT);
+    readyToEscape = (int *)shmat(shmId, NULL, 0);
+    *readyToEscape = 0;
+    shmdt(readyToEscape);
 
-  shmId = shmget(key + 3, sizeof(int), 0600 | IPC_CREAT);
-  freeServiceTrack = (int *)shmat(shmId, NULL, 0);
-  *freeServiceTrack = TRUE;
-  shmdt(freeServiceTrack);
-  // SEMAPHORES
-  semId = semget(key, 3, 0600 | IPC_CREAT);
-  semctl(semId, acces, SETVAL, 1);
-  semctl(semId, queueExit, SETVAL, 0);
-  semctl(semId, queueEntry, SETVAL, 0);
+    shmId = shmget(key + 3, sizeof(int), 0600 | IPC_CREAT);
+    freeServiceTrack = (int *)shmat(shmId, NULL, 0);
+    *freeServiceTrack = TRUE;
+    shmdt(freeServiceTrack);
+    // SEMAPHORES
+    semId = semget(key, 3, 0600 | IPC_CREAT);
+    semctl(semId, acces, SETVAL, 1);
+    semctl(semId, queueExit, SETVAL, 0);
+    semctl(semId, queueEntry, SETVAL, 0);
 
 }
 
 void prepareSharedMemory()
 {
-  int shmId;
+    int shmId;
 
-  shmId = shmget(key, sizeof(int), 0600 | IPC_CREAT);
-  inService = (int *)shmat(shmId, NULL, 0);
+    shmId = shmget(key, sizeof(int), 0600 | IPC_CREAT);
+    inService = (int *)shmat(shmId, NULL, 0);
 
-  shmId = shmget(key + 1, sizeof(int), 0600 | IPC_CREAT);
-  readyToEntry = (int *)shmat(shmId, NULL, 0);
+    shmId = shmget(key + 1, sizeof(int), 0600 | IPC_CREAT);
+    readyToEntry = (int *)shmat(shmId, NULL, 0);
 
-  shmId = shmget(key + 2, sizeof(int), 0600 | IPC_CREAT);
-  readyToEscape = (int *)shmat(shmId, NULL, 0);
+    shmId = shmget(key + 2, sizeof(int), 0600 | IPC_CREAT);
+    readyToEscape = (int *)shmat(shmId, NULL, 0);
 
-  shmId = shmget(key + 3, sizeof(int), 0600 | IPC_CREAT);
-  freeServiceTrack = (int *)shmat(shmId, NULL, 0);
+    shmId = shmget(key + 3, sizeof(int), 0600 | IPC_CREAT);
+    freeServiceTrack = (int *)shmat(shmId, NULL, 0);
 
 }
 
 int runTasks()
 {
-  int pid;
-  int i;
-  for(i = 0; i < N; ++i)
-  {
-    pid = fork();
-    if (pid == 0) // child
+    int pid;
+    int i;
+    for (i = 0; i < N; ++i)
     {
-      prepareSharedMemory();
-      // go!
-      bolid();
-      // finish!
-      return 1;
+        pid = fork();
+        if (pid == 0) // child
+        {
+            prepareSharedMemory();
+            // go!
+            bolid();
+            // finish!
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
     }
-    else
-    {
-      return 0;
-    }
-  }
 }
 
 void endWork()
 {
     int processes;
-    for(processes = N; processes > 0; --processes) // one pass
+    for (processes = N; processes > 0; --processes) // one pass
     {
-      wait(NULL);
+        wait(NULL);
     }
 
     semctl(semId, 0, IPC_RMID);
@@ -149,33 +149,33 @@ void endWork()
 
 void P(int semaphore)
 {
-  semOpArg.sem_num = semaphore;
-  semOpArg.sem_op = -1;
-  semop(semId, &semOpArg, 1);
+    semOpArg.sem_num = semaphore;
+    semOpArg.sem_op = -1;
+    semop(semId, &semOpArg, 1);
 }
 
 void V(int semaphore)
 {
-  semOpArg.sem_num = semaphore;
-  semOpArg.sem_op = 1;
-  semop(semId, &semOpArg, 1);
+    semOpArg.sem_num = semaphore;
+    semOpArg.sem_op = 1;
+    semop(semId, &semOpArg, 1);
 }
 
 int areValid(int argc, char **argv)
 {
-    if(argc!=5)
+    if (argc != 5)
     {
-      return 0;
+        return 0;
     }
     N = atoi(argv[1]);
-	  K = atoi(argv[2]);
+    K = atoi(argv[2]);
     P = atoi(argv[3]);
     L = atoi(argv[4]);
-    if(N <= 0 || K <= 0 || P <= 0 || L <= 0)
+    if (N <= 0 || K <= 0 || P <= 0 || L <= 0)
     {
-      return 0;
+        return 0;
     }
     // key set
     key_t key = ftok(argv[0], 1);
-		return 1;
+    return 1;
 }
